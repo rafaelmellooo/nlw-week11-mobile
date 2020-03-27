de62dd3daeaf7fa96b5a8917968dcae694826e61
Rafael de Mello e Sousa <rafaelmellooo@rafaelmellooo>
2020-03-27 02:32:53 -0300

    Added the opening of whatsapp and email for communication

--- src/pages/Info/index.tsx
+++ src/pages/Info/index.tsx
@@ -1,5 +1,6 @@
 import { Feather } from '@expo/vector-icons';
 import { useNavigation } from '@react-navigation/native';
+import * as MailComposer from 'expo-mail-composer';
 import React from 'react';
 import { Image, Linking, Text, TouchableOpacity, View } from 'react-native';
 // @ts-ignore
@@ -8,9 +9,22 @@ import styles from './styles';
 
 const Info: React.FC = () => {
   const navigation = useNavigation();
+  const message = 'Olá APAD, estou entrando em contato pois gostaria de ajudar no caso "Cadelinha atropelada" com o valor de R$ 120,00';
 
   const navigateBack = () => {
     navigation.goBack();
+  };
+
+  const sendMail = () => {
+    MailComposer.composeAsync({
+      subject: 'Herói do caso: Cadelinha atropelada',
+      recipients: ['rafaelmello0715@outlook.com'],
+      body: message,
+    });
+  };
+
+  const sendWhatsapp = () => {
+    Linking.openURL(`whatsapp://send?phone=+5513991051395&text=${message}`);
   };
 
   return (
